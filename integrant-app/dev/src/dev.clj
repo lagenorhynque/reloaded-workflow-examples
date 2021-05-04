c71470ea74261e84dc0e4e4edd83d150c1d90ea2
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2021-05-04 15:14:59 +0900

    Introduce Integrant

---
+++ integrant-app/dev/src/dev.clj
@@ -0,0 +1,11 @@
+(ns dev
+  (:require
+   [clojure.java.io :as io]
+   [clojure.repl :refer :all]
+   [clojure.spec.alpha :as s]
+   [clojure.tools.namespace.repl :refer [refresh]]
+   [integrant.core :as ig]
+   [integrant.repl :refer [clear halt go init prep reset]]
+   [integrant.repl.state :refer [config system]]))
+
+(clojure.tools.namespace.repl/set-refresh-dirs "dev/src" "src" "test")
--- integrant-app/project.clj
+++ integrant-app/project.clj
@@ -3,8 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[integrant "0.8.0"]
+                 [org.clojure/clojure "1.10.3"]
+                 [ring/ring-core "1.9.3"]
+                 [ring/ring-jetty-adapter "1.9.3"]]
   :main ^:skip-aot integrant-app.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
+             :dev {:source-paths ["dev/src"]
+                   :resource-paths ["dev/resources"]
+                   :dependencies [[integrant/repl "0.3.2"]]}
+             :repl {:repl-options {:init-ns user}}})
