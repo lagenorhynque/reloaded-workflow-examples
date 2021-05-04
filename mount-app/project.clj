ea6b79992483535a3d91535923c60fc1e314e6e4
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2021-05-04 20:31:27 +0900

    Introduce mount

--- mount-app/project.clj
+++ mount-app/project.clj
@@ -3,8 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[mount "0.1.16"]
+                 [org.clojure/clojure "1.10.3"]
+                 [ring/ring-core "1.9.3"]
+                 [ring/ring-jetty-adapter "1.9.3"]]
   :main ^:skip-aot mount-app.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
+             :dev {:source-paths ["dev/src"]
+                   :resource-paths ["dev/resources"]
+                   :dependencies [[org.clojure/tools.namespace "1.1.0"]]}
+             :repl {:repl-options {:init-ns user}}})
